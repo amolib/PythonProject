61ffb19a147d7246b295ae0bb6fa83157f3242f5
amol <amol.deshmukh@infobeans.com>
2023-04-24 11:41:33 +0530

    modified loactors.py file

--- locators.py
+++ locators.py
@@ -15,9 +15,15 @@ driver.find_element(By.ID,"exampleInputPassword1").send_keys("12345")
 driver.find_element(By.ID,"exampleCheck1").click()
 
 # Xpath - //tagname[@attribute='value']  -> //input[@type='submit']
+# CSS - tagname[attribute='value'] -> input[type='submit']  -> #id, .classname <-- all are valid CSS Selectors.
+
+driver.find_element(By.CSS_SELECTOR,"label[for='inlineRadio2']").click()
 driver.find_element(By.XPATH,"//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME,"alert-success").text
 
+driver.find_element(By.XPATH,"(//input[@type='text'])[3]").send_keys("HelloAgain")
+
+
 print(message)
 #driver.implicitly_wait(20)
-driver.close()
+#driver.close()

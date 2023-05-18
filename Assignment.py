4ddaacc0b9e4dd3849a5020f1aa515424b8ed779
amol <amol.deshmukh@infobeans.com>
2023-05-18 14:33:18 +0530

    Add Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,5 +1,7 @@
-import time
 
+
+
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,20 +10,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
+expectedList = ['Cucumber - 1 Kg','Raspberry - 1/4 Kg','Strawberry - 1/4 Kg']
+actualList = []
+
 
 service_obj = Service("C:/Users/Amol Deshmukh/PycharmProjects/chromedriver_win32/chromedriver.exe")
 driver = webdriver.Chrome(options=options, service=service_obj)
-driver.implicitly_wait(2)
+driver.implicitly_wait(4)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.XPATH,"//input[@class='search-keyword']").send_keys("ber")
 time.sleep(2)
+
+
 results = driver.find_elements(By.XPATH,"//div[@class='products']/div")
 count = len(results)
 #print(count)
 assert count > 0
 for result in results:
+    actualList.append(result.find_element(By.XPATH,"h4").text)
     result.find_element(By.XPATH,"div/button").click()
+
+assert expectedList == actualList
+
 driver.find_element(By.CSS_SELECTOR,"img[alt='Cart']").click()
 driver.find_element(By.XPATH,"//button[text()='PROCEED TO CHECKOUT']").click()
 #Sum Validation
@@ -30,10 +41,15 @@ sum = 0
 for price in prices:
     sum = sum + int(price.text)
 print(sum)
-assert sum == int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+assert sum == totalAmount
 
 driver.find_element(By.CSS_SELECTOR,".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR,".promoBtn").click()
 wait = WebDriverWait(driver,10)
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
 print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
+
+discountedAmount = float(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
+assert totalAmount > discountedAmount
+
